fea8e890923820e1cead0c770abbf2de6a9bd7b8
BT_2S14_24 <wowamazingyesofcourse@gmail.com>
2024-11-12 10:57:25 -0500

    Redesigned my game a little, I’ll update the planning sheet later with my idea. Wrote out all the Actions that could happen in the game. Figured out how to print out the field itself with a 2D array from a class instead so I can update the field later. Added a method to calculate distance from goal to determine the success chance of a shot.

--- src/Actions.java
+++ src/Actions.java
@@ -1,2 +1,60 @@
 public class Actions {
+
+    FieldOverview action = new FieldOverview();
+    int distance = action.distanceToGoal();
+
+    public String actions(String action)
+    {
+        String result = "";
+        int extent = ((int)(Math.random() * 3) +1);
+
+        if(action.equals("dribble"))
+        {
+            if( extent == 1) {
+                result += "You've been stopped, no ground gained!";
+                return result;
+            }
+            if( extent == 2)
+            {
+                result += "You've dribbled past and gained 1 square!";
+                return result;
+            }
+            if( extent == 3)
+            {
+                result += "You've dribbled past and gained 2 squares!";
+                return result;
+            }
+        }
+        else if(action.equals("run"))
+        {
+            if( extent == 1)
+            {
+                result += "A defender has come in front, go ground gained!";
+                return result;
+            }
+            else if( extent == 2)
+            {
+                result += "You've ran enough for 1 square!";
+                return result;
+            }
+            else
+            {
+                result += "You've sprinted forward 2 squares!";
+                return result;
+            }
+        }
+        else if(action.equals("shoot"))
+        {
+            extent *= 10;
+            if(extent > distance)
+            {
+                result += "You've scored, great job!";
+            }
+            else
+            {
+                result += "You've missed!";
+            }
+        }
+        return result;
+    }
 }
